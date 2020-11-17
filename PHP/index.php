ce56c30186122916a424411a6927277c830d2a94
cotoski <cotoski@gmail.com>
2020-11-17 15:15:19 -0300

    PHP TESTE

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,13 @@
-<html>
-<body>
-<h12>Meu primeiro php 2<h12>
-<? %TIME& ?>
-</body>
-</html>
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+?>
+<h3>strpos() must have returned non-false</h3>
+<p>You are using Internet Explorer</p>
+<?php
+} else {
+?>
+<h3>strpos() must have returned false</h3>
+<p>You are not using Internet Explorer</p>
+<?php
+}
+?>
